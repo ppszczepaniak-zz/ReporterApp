952ecc3c7fddd9ed2a6fe2611aafaeddd96f2137
pedro-programator <p.p.szczepaniak@gmail.com>
2019-08-03 13:50:11 +0200

    refactor

--- src/main/java/websites/CustomWebsite.java
+++ src/main/java/websites/CustomWebsite.java
@@ -6,24 +6,24 @@ public class CustomWebsite extends Website {
     public void countAndMark(String msg) {
         long score = 0;
         StringBuilder stringBuilder = new StringBuilder();
-        try{
+        try {
             if (msg.length() > 0) {
-            msg = msg.toLowerCase();
-            for (int i = 0; i < msg.length(); i++) {
+                msg = msg.toLowerCase();
+                for (int i = 0; i < msg.length(); i++) {
 
-                if (String.valueOf(msg.charAt(i)).matches("[a-kA-K]")) {
-                    score++;
-                    stringBuilder.append(String.valueOf(msg.charAt(i)).toUpperCase());
-                } else {
-                    stringBuilder.append(msg.charAt(i));
+                    if (String.valueOf(msg.charAt(i)).matches("[a-kA-K]")) {
+                        score++;
+                        stringBuilder.append(String.valueOf(msg.charAt(i)).toUpperCase());
+                    } else {
+                        stringBuilder.append(msg.charAt(i));
+                    }
                 }
             }
-        }
-        } catch (NullPointerException e){
+        } catch (NullPointerException e) {
             System.out.println("Komunikat błędu: " + e.getMessage());
         }
         setScore(score);
         setConvertedMsg(stringBuilder.toString());
-    
+
     }
 }
