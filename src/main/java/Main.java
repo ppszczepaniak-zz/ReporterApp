1980d35e12f8f3cd63e1d3f75f7cb52a98cb06ee
marcinurbaniak <marcin.urbaniak@gmail.com>
2019-08-03 13:04:27 +0200

    add countScore and apitalizeChosenLetters

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,12 @@
+import websites.CustomWebsite;
+
 public class Main {
+    public static void main(String[] args) {
+        CustomWebsite customWebsite = new CustomWebsite();
+
+        System.out.println(customWebsite.capitalizeChosenLetters("Ala ma kota"));
+    }
+
 }
+
+
--- src/main/java/websites/CustomWebsite.java
+++ src/main/java/websites/CustomWebsite.java
@@ -9,14 +9,40 @@ public class CustomWebsite extends Website {
 
     @Override
     public long countScore(String msg) {
+        if (!msg.matches("[a-kA-K]") || msg.equals(null))return 0;
 
 
+        long score = 0;
+        for (int i = 0; i < msg.length() ; i++) {
+            if(String.valueOf(msg.charAt(i)).matches("a-kA-K")){
+                score++;
 
-        return 0 ;
+            }
+
+        }
+
+
+        return score ;
     }
 
     @Override
     public String capitalizeChosenLetters(String msg) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < msg.length(); i++) {
+            if(String.valueOf(msg.charAt(i)).matches("[a-kA-K]")){
+                stringBuilder.append(String.valueOf(msg.charAt(i)).toUpperCase());
+
+            }
+            else {
+                stringBuilder.append(msg.charAt(i));
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public String toString() {
+        return "CustomWebsite{}";
     }
 }
