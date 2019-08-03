3c9d5b2d0b8daddd1250b5b60463c96412818665
pedro-programator <p.p.szczepaniak@gmail.com>
2019-08-03 12:52:10 +0200

    add count and score to VowelWebsite

--- src/main/java/websites/VowelWebsite.java
+++ src/main/java/websites/VowelWebsite.java
@@ -2,10 +2,33 @@ package websites;
 
 public class VowelWebsite extends Website {
 
-    //test
+    public void countAndMark(String msg) {
+        long count = 0;
+        StringBuilder newMsg = new StringBuilder();
+
+        //Converting entire string to lower case to reduce the comparisons
+        msg = msg.toLowerCase();
+
+        if (msg.length() > 0) {
+            for (int i = 0; i < msg.length(); i++) {
+                char ch = msg.charAt(i);
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    count++;
+                    newMsg.append(String.valueOf(msg.charAt(i)).toUpperCase());
+                } else {
+                    newMsg.append(msg.charAt(i));
+                }
+            }
+        }
+
+        setScore(count);
+        setConvertedMsg(newMsg.toString());
+    }
+
 
     @Override
     public long countScore(String msg) {
+
         return 0;
     }
 
