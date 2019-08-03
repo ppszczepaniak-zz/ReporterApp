5540c0c4980cb7943f792b69f1139d977f6e36b5
marcinurbaniak <marcin.urbaniak@gmail.com>
2019-08-03 11:46:12 +0200

    add constructor to Website class

--- src/main/java/websites/Website.java
+++ src/main/java/websites/Website.java
@@ -2,10 +2,29 @@ package websites;
 
 public abstract class Website {
 
-    protected long score;
-    protected String convertedMsg;
+    private long score;
+    private String convertedMsg;
+
+
 
     protected abstract long countScore(String msg);
     protected abstract String capitalizeChosenLetters(String msg);
+
+    public long getScore() {
+        return score;
+    }
+
+    public void setScore(long score) {
+        this.score = score;
+    }
+
+    public String getConvertedMsg() {
+        return convertedMsg;
+    }
+
+    public void setConvertedMsg(String convertedMsg) {
+        this.convertedMsg = convertedMsg;
+    }
+
 
 }
