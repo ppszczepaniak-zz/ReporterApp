ab649d156ca3df269e248a9c46571bbfdcd5ecc7
pedro-programator <p.p.szczepaniak@gmail.com>
2019-08-03 15:30:38 +0200

    add main method with working reporter and websites

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,26 @@
+import reporter.Reporter;
+import websites.ConsonantWebsite;
+import websites.CustomWebsite;
+import websites.VowelWebsite;
+
 public class Main {
+    public static void main(String[] args) {
+        //W klasie Main należy stworzyć obiekt reportera,
+        // który współpracuje z różnymi typami portali i wywołać dla niego metodę broadcastMessage .
+
+        Reporter reporter = new Reporter();
+
+        VowelWebsite vowelWebsite = new VowelWebsite();
+        ConsonantWebsite consonantWebsite = new ConsonantWebsite();
+        CustomWebsite customWebsite = new CustomWebsite();
+
+        reporter.addWebsite(vowelWebsite);
+        reporter.addWebsite(consonantWebsite);
+        reporter.addWebsite(customWebsite);
+
+        reporter.broadcastMessage("Cows lose their jobs as milk prices drop");
+
+    }
 }
 
 
--- src/main/java/reporter/Reporter.java
+++ src/main/java/reporter/Reporter.java
@@ -9,14 +9,14 @@ public class Reporter implements Broadcast {
 
     protected List<Website> websites = new ArrayList<>();
 
-    public void addWebsite(Website website){
-        if (website != null){
+    public void addWebsite(Website website) {
+        if (website != null) {
             websites.add(website);
         }
     }
 
-    public void deleteWebsite(Website website){
-        if(website != null){
+    public void deleteWebsite(Website website) {
+        if (website != null) {
             websites.remove(website);
         }
     }
@@ -24,12 +24,12 @@ public class Reporter implements Broadcast {
 
     @Override
     public void broadcastMessage(String msg) {
-        for (Website website: websites
-             ) {
-            if(website instanceof Website){
+        for (Website website : websites) {
+            if (website instanceof Website) {
                 website.outputMessage(msg);
             }
-            
-        }   
+        }
     }
+
+
 }
