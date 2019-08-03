813680207eb6df3d6b45a342513a8e6a61418989
pedro-programator <p.p.szczepaniak@gmail.com>
2019-08-03 13:24:22 +0200

    should be ok now

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,27 @@
 import websites.CustomWebsite;
+import websites.VowelWebsite;
 
 public class Main {
     public static void main(String[] args) {
-        CustomWebsite customWebsite = new CustomWebsite();
+        VowelWebsite vowelWebsite = new VowelWebsite();
 
-        System.out.println(customWebsite.capitalizeChosenLetters("Ala ma kota"));
+
+        String str1 = "abCd EfGh";
+        String str2 = "Cows lose their jobs as milk prices drop";
+
+
+        vowelWebsite.countAndMark(str1);
+        System.out.println("Original message: " + str1);
+        System.out.println("Message after: " + vowelWebsite.getConvertedMsg());
+        System.out.println("Score after: " + vowelWebsite.getScore());
+
+        System.out.println("==============");
+        vowelWebsite.countAndMark(str2);
+        System.out.println("Original message: " + str2);
+        System.out.println("Message after: " + vowelWebsite.getConvertedMsg());
+        System.out.println("Score after: " + vowelWebsite.getScore());
+
     }
-
 }
 
 
--- src/main/java/websites/CustomWebsite.java
+++ src/main/java/websites/CustomWebsite.java
@@ -2,47 +2,8 @@ package websites;
 
 public class CustomWebsite extends Website {
 
-    public CustomWebsite() {
-        super();
-
-    }
-
     @Override
-    public long countScore(String msg) {
-        if (!msg.matches("[a-kA-K]") || msg.equals(null))return 0;
+    protected void countAndMark(String msg) {
 
-
-        long score = 0;
-        for (int i = 0; i < msg.length() ; i++) {
-            if(String.valueOf(msg.charAt(i)).matches("a-kA-K")){
-                score++;
-
-            }
-
-        }
-
-
-        return score ;
-    }
-
-    @Override
-    public String capitalizeChosenLetters(String msg) {
-        StringBuilder stringBuilder = new StringBuilder();
-
-        for (int i = 0; i < msg.length(); i++) {
-            if(String.valueOf(msg.charAt(i)).matches("[a-kA-K]")){
-                stringBuilder.append(String.valueOf(msg.charAt(i)).toUpperCase());
-
-            }
-            else {
-                stringBuilder.append(msg.charAt(i));
-            }
-        }
-        return stringBuilder.toString();
-    }
-
-    @Override
-    public String toString() {
-        return "CustomWebsite{}";
     }
 }
